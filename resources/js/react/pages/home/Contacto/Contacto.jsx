df7a420007eb8278ce8477b0b4b26288397af81d
Epsilonxaos <angelotr12@hotmail.com>
2025-02-03 14:54:53 -0600

    feat(Web): :sparkles: Agregar componente Contacto a la página de inicio

---
+++ resources/js/react/pages/home/Contacto/Contacto.jsx
@@ -0,0 +1,14 @@
+import Container from '@components/Container'
+
+const Contacto = () => {
+	return (
+		<Container>
+			<div className='grid grid-cols-1 lg:grid-cols-2'>
+				<div className='col-span-1'></div>
+				<div className='col-span-1'>s</div>
+			</div>
+		</Container>
+	)
+}
+
+export default Contacto
