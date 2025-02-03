1ce77a272cdfdf8dd866961aab7f58033f563173
Epsilonxaos <angelotr12@hotmail.com>
2025-02-03 14:53:52 -0600

    chore(Web): :wrench: Actualizacion de paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,26 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: [
-		'./public/index.php',
-		'./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-		'./storage/framework/views/*.php',
-		'./resources/**/*.blade.php',
-		'./resources/**/*.{js,ts,jsx,tsx}',
-		'./node_modules/flowbite/**/*.js',
-	],
-	darkMode: 'class',
-	theme: {
-		extend: {
-			colors: {
-				menta: '#6FD0A5',
-				azulMarino: '#005677',
-				azulRey: '#003A5D',
-				verdeLimon: '#D3D655',
-			},
-			fontFamily: {
-				gotham: ['Gotham', 'sans-serif'],
-			},
+export const content = [
+	'./public/index.php',
+	'./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+	'./storage/framework/views/*.php',
+	'./resources/**/*.blade.php',
+	'./resources/**/*.{js,ts,jsx,tsx}',
+	'./resources/js/react/**/*.{js,ts,jsx,tsx}',
+	'./resources/js/react/**/**/*.{js,ts,jsx,tsx}',
+	'./resources/js/react/**/**/**/*.{js,ts,jsx,tsx}',
+	'./node_modules/flowbite/**/*.js',
+]
+export const darkMode = 'class'
+export const theme = {
+	extend: {
+		colors: {
+			menta: '#6FD0A5',
+			azulMarino: '#005677',
+			azulRey: '#003A5D',
+			verdeLimon: '#D3D655',
+		},
+		fontFamily: {
+			gotham: ['Gotham', 'sans-serif'],
 		},
 	},
-
-	plugins: [require('@tailwindcss/forms'), require('flowbite/plugin')],
 }
+export const plugins = [require('@tailwindcss/forms'), require('flowbite/plugin')]
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { readFileSync } from 'fs'
 import laravel from 'laravel-vite-plugin'
+import path from 'path'
 import { defineConfig, loadEnv } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
@@ -44,8 +45,11 @@ export default defineConfig(({ mode }) => {
 		],
 
 		resolve: {
+			// alias: {
+			// 	'@': 'resources/js/react',
+			// },
 			alias: {
-				'@': 'resources/js/react',
+				'@': path.resolve(__dirname, 'resources'),
 			},
 		},
 	}
